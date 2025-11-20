579c481bd2549f2d0b2a7639b72121293fb24163
itzjeth <jjabay3@gmail.com>
2025-11-20 19:55:46 +0800

    commit6

--- sting/settings.py
+++ sting/settings.py
@@ -25,8 +25,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # SECURITY
 SECRET_KEY = 'django-insecure-(0zdy7ad-m%b%$0geg^i-f78=!0$tea!@^=l(p#7=hwa@^2wt*'
-DEBUG = True
-ALLOWED_HOSTS = ['https://stingtodeploy.onrender.com/']  # Allow all hosts for Render deployment
+DEBUG = False
+ALLOWED_HOSTS = ['stingtodeploy.onrender.com', 'localhost', '127.0.0.1']
+  # Allow all hosts for Render deployment
 
 # Email configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
